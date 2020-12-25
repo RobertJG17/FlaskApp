46b65aa0b80a3ca2a33919990acb87ead6a60017
Robert Guerra <robertjguerra@yahoo.com>
2020-12-25 01:52:10 -0800

    Refactored, cleaned up

--- main.py
+++ main.py
@@ -17,11 +17,12 @@ def set_release_date_precision(df):
     flipped_dates = df['release_date'].apply(flip_date)
     dt_objects = pd.to_datetime(flipped_dates)
     days = dt_objects.dt.day_name()
+    print(type(days))
     df['release_date_precision'] = days
 
 
 def is_in_us(countries):
-    return 'US' in countries
+    return 'US' not in countries
 
 
 # Reading information into a DataFrame
@@ -31,11 +32,11 @@ album_dataframe = pd.DataFrame(spotty.albums)
 album_dataframe = album_dataframe.drop(['artists', 'album_group', 'album_type', 'external_urls',
                                         'href', 'id', 'images', 'type'], axis=1)
 
+# Filter the albums that were solely a US release
 us_based_albums = album_dataframe[album_dataframe['available_markets'].apply(is_in_us)]
 us_based_albums.index = np.arange(len(us_based_albums))
 set_release_date_precision(us_based_albums)
 uba = us_based_albums.to_html()
-
 
 # Setting specific day for release date precision column
 set_release_date_precision(album_dataframe)
