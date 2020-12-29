8edabff03593298e00b6116f023563e728d58e7c
Robert Guerra <robertjguerra@yahoo.com>
2020-12-29 00:23:56 -0800

    added cors import to handle heroku app access from react

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
 from flask import Flask, current_app
 from analytics import items_html, popular_html, genre_json, artists_json
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/": {"origins": ""}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/', methods=['GET'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def index():
     return current_app.send_static_file('top_artists.html')
 
 
 @app.route('/items', methods=['GET'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def items():
     return items_html
 
 
 @app.route('/popular', methods=['GET'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def popular():
     return popular_html
 
 
 @app.route('/genres', methods=['GET'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def genres():
     return genre_json
 
 
 @app.route('/artists', methods=['GET'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def artists():
     return artists_json
 
